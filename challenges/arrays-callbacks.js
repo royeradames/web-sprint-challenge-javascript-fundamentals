477fac787fb6b4d3319d3cd525f46a1ac33b244b
royeradames <royeraadames@gmail.com>
2020-06-19 12:18:09 -0400

    finished arrays callback up to reduce()

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(item){
+  displayNames.push({animal_name: item.animal_name, scientific_name: item.scientific_name});
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +33,11 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(function(item){
+  
+  return {animal_name: item.animal_name.toLowerCase()};
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +45,10 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+
+const lowPopulationAnimals = zooAnimals.filter(function(items){
+  return items.population < 5;
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -46,6 +57,9 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 let populationTotal = 0;
+populationTotal = zooAnimals.reduce(function(accumulator, item){
+  return accumulator + item.population;
+},0);
 console.log(populationTotal);
 
 
